7381be2ea861bfa786f796b1ed6989fb2fd34ecd
Ruslan <ruslansolodovnyk@gmail.com>
2018-12-11 23:29:07 +0200

    Complete task 11

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -1,2 +1,63 @@
 package com.example.background.workers
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.util.Log
+
+import com.example.background.KEY_IMAGE_URI
+
+import java.io.FileNotFoundException
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+
+    private val TAG by lazy { BlurWorker::class.java.simpleName }
+
+    override fun doWork(): Result {
+        val appContext = applicationContext
+
+        // Makes a notification when the work starts and slows down the work so that it's easier to
+        // see each WorkRequest start, even on emulated devices
+        makeStatusNotification("Blurring image", appContext)
+        sleep()
+
+        return try {
+            createBlurredBitmap(appContext, inputData.getString(KEY_IMAGE_URI))
+            Result.SUCCESS
+        } catch (fileNotFoundException: FileNotFoundException) {
+            Log.e(TAG, "Failed to decode input stream", fileNotFoundException)
+            throw RuntimeException("Failed to decode input stream", fileNotFoundException)
+        } catch (throwable: Throwable) {
+            // If there were errors, return FAILURE
+            Log.e(TAG, "Error applying blur", throwable)
+            Result.FAILURE
+        }
+    }
+
+    @Throws(FileNotFoundException::class, IllegalArgumentException::class)
+    private fun createBlurredBitmap(appContext: Context, resourceUri: String?) {
+        if (resourceUri.isNullOrEmpty()) {
+            Log.e(TAG, "Invalid input uri")
+            throw IllegalArgumentException("Invalid input uri")
+        }
+
+        val resolver = appContext.contentResolver
+
+        // Create a bitmap
+        val bitmap = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri)))
+
+        // Blur the bitmap
+        val output = blurBitmap(bitmap, appContext)
+
+        // Write bitmap to a temp file
+        val outputUri = writeBitmapToFile(appContext, output)
+
+        // Return the output for the temp file
+        outputData = Data.Builder().putString(KEY_IMAGE_URI, outputUri.toString()
+        ).build()
+    }
+}
